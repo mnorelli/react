7dd92a3bd4cd86b784d907dd6fa6a479c786c2b6
Michael Norelli <mnorelli360@gmail.com>
2016-07-12 16:14:58 -0700

    add another component

---
+++ starter-code/app/components/greeting.jsx
@@ -0,0 +1,13 @@
+// greet component...
+import React from 'react'
+
+export default class Greeting extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Good {this.props.timeOfDay}, {this.props.object}!</h3>
+        <p>So good!</p>
+      </div>
+      )
+  }
+}
--- starter-code/app/index.jsx
+++ starter-code/app/index.jsx
@@ -3,8 +3,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import HelloWorld from './components/hello-world'
+import Greeting  from './components/greeting'
 
 
 ReactDOM.render(
   <HelloWorld name="WDI"/>,document.getElementById("hello-world-component")
   )
+
+ReactDOM.render(
+  <Greeting timeOfDay="night" object="moon"/>,document.getElementById("greet")
+  )
+
+
+
+
+
